8e70dc7762b6400c6af0c2e70bb9da21b97c13b4
TJ <64841938+thegitaccount@users.noreply.github.com>
2023-05-06 21:04:02 +0300

    new commit

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,25 @@
+import { useDispatch } from 'react-redux'
+import { newAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+    const dispatch = useDispatch()
+
+const addAnecdote = async (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch(newAnecdote(content))
+}
+
+    return (
+        <div>
+        <h2>create new</h2>
+        <form onSubmit={addAnecdote}>
+            <div><input name="anecdote" /></div>
+            <button type="submit">Add anecdote</button>
+        </form>
+        </div>
+    )
+}
+
+export default AnecdoteForm
